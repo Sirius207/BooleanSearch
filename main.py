80b6b001a30debe98df1031568d2d4915e402a90
Po Chun, Lu <oceanus11034@gmail.com>
2018-03-23 18:50:33 +0800

    [Init] sample data

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+if __name__ == '__main__':
+    # You should not modify this part.
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source',
+                       default='source.csv',
+                       help='input source data file name')
+    parser.add_argument('--query',
+                        default='query.txt',
+                        help='query file name')
+    parser.add_argument('--output',
+                        default='output.txt',
+                        help='output file name')
+    args = parser.parse_args()
+
+    # Import Data
+    Source = pd.read_csv(args.source, names = ["ID", "Title"])
+    
+    
+    # Create Index
+    
+
+    # Import Query
+    
+
+    # Searching & Output
+
+
+
+
